92e9e7aab190f982b2034dd7116a84176e0b94a4
Federico <federuedaleal@hotmail.com>
2023-08-27 00:54:07 -0300

    Se corrigen las rutas al back

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -14,7 +14,6 @@ const Login = ({ setUsuarioLogueado }) => {
     reset,
   } = useForm();
   const navegacion = useNavigate();
-
   const onSubmit = (usuario) => {
     iniciarSesion(usuario).then((respuesta) => {
       if (respuesta) {
